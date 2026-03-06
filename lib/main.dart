c018a35bf5331f499b013e1f8d4b2c8b8ec43c2b
Nikita Milovsky <karaskarasevic1@gmail.com>
2026-03-06 19:35:24 +0400

    add basic map

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:mirly/examples/bicycle_page.dart';
+import 'package:mirly/examples/circle_map_object_page.dart';
+import 'package:mirly/examples/clusterized_placemark_collection_page.dart';
+import 'package:mirly/examples/driving_page.dart';
+import 'package:mirly/examples/map_controls_page.dart';
+import 'package:mirly/examples/map_object_collection_page.dart';
+import 'package:mirly/examples/placemark_map_object_page.dart';
+import 'package:mirly/examples/polygon_map_object_page.dart';
+import 'package:mirly/examples/polyline_map_object_page.dart';
+import 'package:mirly/examples/reverse_search_page.dart';
+import 'package:mirly/examples/search_page.dart';
+import 'package:mirly/examples/suggest_page.dart';
+import 'package:mirly/examples/user_layer_page.dart';
+import 'package:mirly/examples/widgets/map_page.dart';
+import 'package:yandex_mapkit/yandex_mapkit.dart';
 
 void main() {
-  runApp(const MyApp());
+  AndroidYandexMap.useAndroidViewSurface = false;
+  runApp(const MaterialApp(home: MainPage()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+const List<MapPage> _allPages = <MapPage>[
+  MapControlsPage(),
+  ClusterizedPlacemarkCollectionPage(),
+  MapObjectCollectionPage(),
+  PlacemarkMapObjectPage(),
+  PolylineMapObjectPage(),
+  PolygonMapObjectPage(),
+  CircleMapObjectPage(),
+  UserLayerPage(),
+  SuggestionsPage(),
+  SearchPage(),
+  ReverseSearchPage(),
+  BicyclePage(),
+  DrivingPage(),
+];
 
-  // This widget is the root of your application.
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  void _pushPage(BuildContext context, MapPage page) {
+    Navigator.push(
+      context,
+      MaterialPageRoute<void>(
+        builder: (_) => Scaffold(
+          appBar: AppBar(title: Text(page.title)),
+          body: Container(padding: const EdgeInsets.all(8), child: page),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text("Test"));
+    return Scaffold(
+      appBar: AppBar(title: const Text('YandexMap examples')),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              child: YandexMap(),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _allPages.length,
+              itemBuilder: (_, int index) => ListTile(
+                title: Text(_allPages[index].title),
+                onTap: (() => _pushPage(context, _allPages[index])),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
